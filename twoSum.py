8ec2f9e0305909433133d7ba2103479ffa1d251d
yusei399 <yusei2021@keio.jp>
2022-09-11 22:11:52 +0900

    1

---
+++ twoSum.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        result = []
+        for i in range(0, len(nums)-1):
+            for j in range(i + 1, len(nums)):
+                if (nums[i] + nums[j])== target:
+                    result.append(i)
+                    result.append(j)
+                    break
+        return result
