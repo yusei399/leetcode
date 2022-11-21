ce0e44516f430ca4fc4052df706991a6afc2d6b9
yusei399 <yusei2021@keio.jp>
2022-11-21 23:21:36 +0900

    sameTre

---
+++ sameTree.py
@@ -0,0 +1,9 @@
+from typing import Optional
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return 1
+        if not p or not q:
+            return 0
+        return p.val==q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
