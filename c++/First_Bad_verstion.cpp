6f20ccf71e588ded7e70a569ad007c6dd298d0c5
yusei399 <yusei2021@keio.jp>
2023-02-27 18:45:50 +0900

    c++

---
+++ c++/First_Bad_verstion.cpp
@@ -0,0 +1,27 @@
+// The API isBadVersion is defined for you.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int s = 0;
+        int e = n;
+        int mid = s + (e-s)/2;
+        while(s<=e){
+            if(isBadVersion(mid)==true){
+               if(isBadVersion(mid-1)==false){
+                    return mid;
+               }
+            }
+            if(isBadVersion(mid)==false){
+                s = mid+1;
+            }
+            else{
+                e = mid - 1;
+            }
+            mid = s + (e-s)/2;
+        }
+        
+        return -1;
+    }
+};
